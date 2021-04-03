ccc83c3f77f182e1b4d74530fb98afb1672d7478
Pedro Ogheneochuko <pedroogheneochuko@gmail.com>
2021-04-03 01:34:43 +0100

    Still trying to connect to db

--- db-connect.php
+++ db-connect.php
@@ -1,7 +1,7 @@
 <?php   
    $dbhost = 'sql104.epizy.com';
    $dbuser = 'epiz_28296413';
-   $dbpass = 'password';
+   $dbpass = '9NPT3kI68V';
    
    $conn= mysqli_connect("$dbhost","$dbuser","$dbpass") or die ("could not connect to mysql");
    mysqli_select_db($conn, "epiz_28296413_bluefrog");
